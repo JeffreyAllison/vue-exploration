e1af9b60d1644c4edc69c9c9018d0a9f28fb5126
JeffAllison <itsjallison@gmail.com>
2022-07-25 16:33:24 -0700

    added about, home, list, page folders to components folder, added header and layout jsx files to page folder

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,3 +1,10 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 export default function App() {
   return <div>Hello World, hello kitty, hello clarice</div>;
 }
