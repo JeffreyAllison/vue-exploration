a0270f8657bf5a87c435acb2727e6a2333ade405
JeffAllison <itsjallison@gmail.com>
2022-07-25 16:36:32 -0700

    added about, home, and list jsx files in respective folders

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,13 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import Layout from './Page/Layout.jsx';
+import Home from './Home/Home.jsx';
+import List from './List/List.jsx';
+import About from './About/About.jsx';
 
 export default function App() {
-  return <div>Hello World, hello kitty, hello clarice</div>;
+  return (
+    <Router></Router>
+  <div>Hello World, hello kitty, hello clarice</div>);
 }
