b199b83f6aa61fb6e49261ff2fba454e34ab83e6
JeffAllison <itsjallison@gmail.com>
2022-07-26 19:01:28 -0700

    updated home.jsx, updated App.jsx name in github

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import styles from './Home.css';
 export default function Home() {
   return (
     <div className={styles.Home}>
-      <p>Hello World, hello kitty, hello clarice</p>
+      <p>Hello World!</p>
     </div>
   );
 }
