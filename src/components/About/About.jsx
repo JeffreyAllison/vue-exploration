c33cd8f21a17e24d9cc2058dea2c404c885474cc
JeffAllison <itsjallison@gmail.com>
2022-07-27 17:32:09 -0700

    added padding styling to list css, created form file, created form css and jsx f file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,53 @@
+import {
+  InputControl,
+  SelectControl,
+  TextAreaControl,
+  CheckboxControl,
+  FormButton,
+  Fieldset,
+} from '../Forms/Form.jsx';
+
 import styles from './About.css';
 
 export default function About() {
   return (
-    <div className={styles.About}>
-      <p>About page about page about page</p>
-    </div>
+    <section className={styles.About}>
+      about page about page about page now fill out the form
+      <form>
+        <Fieldset legend="your info here">
+          <InputControl
+            label="email"
+            name="email"
+            placeholder="enter your email"
+            type="email"
+            required
+          />
+
+          <InputControl
+            className={styles.PasswordControl}
+            label="password"
+            name="password"
+            placeholder="choose a password"
+            type="password"
+            required
+          />
+        </Fieldset>
+
+        <SelectControl label="type">
+          <option>Alien</option>
+          <option>Boolean</option>
+          <option>Cat</option>
+        </SelectControl>
+
+        <CheckboxControl label="Easy Mode?" text="No thx" />
+
+        <TextAreaControl
+          label="jeff's bio"
+          placeholder="tell us about you jeff"
+        />
+
+        <FormButton>Submit</FormButton>
+      </form>
+    </section>
   );
 }
