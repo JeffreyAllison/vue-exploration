21d2ebc93b0c1b6749f012dccc653d54fda52313
JeffAllison <itsjallison@gmail.com>
2022-08-01 16:25:43 -0700

    added workshops and participantsworkshops components files, added them as routes, and the workshop provider wrapper to app.jsx router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,19 +9,30 @@ import Home from './Home/Home.jsx';
 import List from './List/List.jsx';
 import About from './About/About.jsx';
 import Pokedex from './Pokedex/Pokedex.jsx';
+import WorkshopProvider from '../../state/context/WorkshopsContext.jsx';
+import ParticipantsWorkshops from './Workshops/ParticipantsWorkshops.jsx';
+import Workshops from './Workshops/Workshops.jsx';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="list" element={<List />} />
-          <Route path="about" element={<About />} />
-          <Route path="pokedex" element={<Pokedex />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+      <WorkshopProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="list" element={<List />} />
+            <Route path="about" element={<About />} />
+            <Route path="pokedex" element={<Pokedex />} />
+            <Route
+              path="participants-workshops"
+              element={<ParticipantsWorkshops />}
+            >
+              <Route index element={<Workshops />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </WorkshopProvider>
     </Router>
   );
 }
