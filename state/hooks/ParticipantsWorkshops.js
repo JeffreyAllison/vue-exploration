e0c1f567713e729bf82e6a3679ad394afcf5c21b
JeffAllison <itsjallison@gmail.com>
2022-08-01 18:11:04 -0700

    created dispatch actions and workshop actions functions to utitlise crud ops imported from workshop-service file

--- state/hooks/ParticipantsWorkshops.js
+++ state/hooks/ParticipantsWorkshops.js
@@ -1,2 +1,74 @@
 import { useContext, useEffect, useState } from 'react';
 import { WorkshopContext } from '../context/WorkshopsContext';
+import {
+  getWorkshops,
+  deleteWorkshop,
+  createWorkshop,
+  updateWorkshop,
+} from '../services/workshop-service.js';
+
+export function useWorkshops() {
+  const [error, setError] = useState(null);
+  const { workshops, workshopDispatch } = useContext(WorkshopContext);
+
+  useEffect(() => {
+    if (workshops) return;
+    let ignore = false;
+
+    const fetch = async () => {
+      const { data, error } = await getWorkshops();
+      if (ignore) return;
+
+      if (error) {
+        setError(error);
+      }
+      if (data) {
+        workshopDispatch({ type: 'load', payload: data });
+      }
+    };
+    fetch();
+    return () => (ignore = true);
+  }, []);
+  return { workshops, error };
+}
+
+function createDispatchActions(dispatch) {
+  return function createAction({ service, type, success }) {
+    return async (...args) => {
+      const { data, error } = await service(...args);
+
+      if (error) showError(error.message);
+      if (data) {
+        dispatch({ type, payload: data });
+        const successMessage = success(data);
+        showSuccess(successMessage);
+      }
+    };
+  };
+}
+
+export function useWorkshopActions() {
+  const { workshopDispatch } = useContext(WorkshopContext);
+
+  const createAction = createDispatchActions(workshopDispatch);
+
+  const add = createAction({
+    service: createWorkshop,
+    type: 'add',
+    success: (data) => `Created ${data.topic}`,
+  });
+
+  const remove = createAction({
+    service: deleteWorkshop,
+    type: 'delete',
+    success: (data) => `Deleted ${data.topic}`,
+  });
+
+  const update = createAction({
+    service: updateWorkshop,
+    type: 'update',
+    success: (data) => `Updated ${data.topic}`,
+  });
+
+  return { add, remove, update };
+}
